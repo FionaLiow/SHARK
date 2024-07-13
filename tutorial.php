e30601b0ef95bc38efdb987a39e9732b680a22f6
Fiona <tp060687@mail.apu.edu.my>
2024-07-14 00:19:29 +0800

    final :)

--- tutorial.php
+++ tutorial.php
@@ -5,12 +5,14 @@
         table {
             width: 100%;
             border-collapse: collapse;
+            table-layout: fixed;
         }
 
         th,
         td {
             border: 1px solid #ddd;
             padding: 8px;
+            vertical-align: top;
         }
 
         th {
@@ -22,7 +24,7 @@
             text-align: left;
         }
     </style>
-    <h1>&nbsp&nbsp&nbsp Vide<i class="fa-solid fa-compact-disc fa-spin" style="color: #2f2a32;"></i> Playlists</h1></br>
+    <h1>&nbsp Vide<i class="fa-solid fa-compact-disc fa-spin" style="color: #2f2a32;"></i> Playlists</h1></br>
     <table>
         <tr>
             <th>Low</th>
@@ -32,27 +34,44 @@
         <tr>
             <td>
                 <ul>
-                    <li><a href="https://example.com/video1" target="_blank">Video 1</a></li>
-                    <li><a href="https://example.com/video2" target="_blank">Video 2</a></li>
-                    <li><a href="https://example.com/video3" target="_blank">Video 3</a></li>
+                    <li><a href="https://drive.google.com/file/d/1doosFoH7CU2juEzXy-w19jukDqI37uO9/view?usp=drive_link" target="_blank">SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1rHfuhgti7PlD_sa4dZ02VhPSvKS3TbkF/view?usp=drive_link" target="_blank">Blind SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1JvfE4TR4eV_oKyYiFzUWZx9Qp8cPgoOx/view?usp=drive_link" target="_blank">File Inclusion</a></li>
+                    <li><a href="https://drive.google.com/file/d/11hGDJHUKVljKZt4knzFTSnA1JnLBlUbu/view?usp=drive_link" target="_blank">File Upload</a></li>
+                    <li><a href="https://drive.google.com/file/d/1bmvUKccs0Mp64AX6dSi-ZpkIz5Azm_13/view?usp=drive_link" target="_blank">Brute Force</a></li>
+                    <li><a href="https://drive.google.com/file/d/1hEZUvCVqv_2427vEA4OjRa1ojmwtlR6q/view?usp=drive_link" target="_blank">Command Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ZvZ2_VeTbiaSvHn2y2CmcYS8zlg6L0Yh/view?usp=drive_link" target="_blank">Open HTTP Redirect</a></li>
                 </ul>
             </td>
             <td>
                 <ul>
-                    <li><a href="https://example.com/video4" target="_blank">Video 4</a></li>
-                    <li><a href="https://example.com/video5" target="_blank">Video 5</a></li>
-                    <li><a href="https://example.com/video6" target="_blank">Video 6</a></li>
+                    <li><a href="https://drive.google.com/file/d/1kSXjBaarPDg4cQtTOEO4BhPVIA0EYTR2/view?usp=drive_link" target="_blank">SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ftLalNKZRZty5RaQ-NxdRTYOLqU240MQ/view?usp=drive_link" target="_blank">Blind SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/11tyV8Y9niywpYl78z-PSGH5XrA88_ErW/view?usp=drive_link" target="_blank">File Inclusion</a></li>
+                    <li><a href="https://drive.google.com/file/d/1fEd6t3bvsWST9O8l2maU-ynDz86hPsSQ/view?usp=drive_link" target="_blank">File Upload</a></li>
+                    <li><a href="https://drive.google.com/file/d/1Y289W2VR19ENKU0ILCgO8jyVFxPPF4IQ/view?usp=drive_link" target="_blank">Brute Force</a></li>
+                    <li><a href="https://drive.google.com/file/d/1CE2UQv7QQ0UGYMMJ2NiBNt3N4Dn7uXdE/view?usp=drive_link" target="_blank">Command Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1xUuRYC9B6GzzgBkeo4K3yglEiqYG06jd/view?usp=drive_link" target="_blank">Open HTTP Redirect</a></li>
+                    <li><a href="https://drive.google.com/file/d/1Q3Ep1ZcBfFtLoWg1rN5vMeRo7ACFcyvM/view?usp=drive_link" target="_blank">BONUS - Blind SQL Injection Technique with SQL Injection challenge</a></li>
                 </ul>
             </td>
             <td>
                 <ul>
-                    <li><a href="https://example.com/video7" target="_blank">Video 7</a></li>
-                    <li><a href="https://example.com/video8" target="_blank">Video 8</a></li>
-                    <li><a href="https://example.com/video9" target="_blank">Video 9</a></li>
+                    <li><a href="https://drive.google.com/file/d/1mPeRW9CHHUl5sEy2QOuRL657nb-BnnDB/view?usp=drive_link" target="_blank">SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1dgziVEmJekptB1R2AqoyGRdKi9bqggjz/view?usp=drive_link" target="_blank">Blind SQL Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1cQHDVQk7ipKIcIJqZsQ9CqYXLBWsiE_z/view?usp=drive_link" target="_blank">File Inclusion</a></li>
+                    <li><a href="https://drive.google.com/file/d/1ewG4aUXtJvA32yg9xkX2IpjMXHt_pBbZ/view?usp=drive_link" target="_blank">File Upload</a></li>
+                    <li><a href="https://drive.google.com/file/d/106f-6VOYRvxDqlRQDTXO_q6pPrBvVEHc/view?usp=drive_link" target="_blank">Brute Force</a></li>
+                    <li><a href="https://drive.google.com/file/d/1KGuVoCXiDNFzcoxtyQUyZeY1zVgExscs/view?usp=drive_link" target="_blank">Command Injection</a></li>
+                    <li><a href="https://drive.google.com/file/d/1u608Jeu9abgJBXsJhrJQe25HIOPv6M1s/view?usp=drive_link" target="_blank">Open HTTP Redirect</a></li>
                 </ul>
             </td>
         </tr>
     </table>
+    <div class="note" style="display:flex; padding-top:10px;">
+        <h1>&nbsp <i class="fa-solid fa-clover fa-spin" style="color: #04d72e;"></i></h1>
+        <h4 style="font-weight: normal; padding-top:10px;">&nbsp The demo above are just some basic exploitation ways, <span style="font-weight: bold;">GOOD LUCK</span> exploring on your own :)</h4>
+    </div>
 
 </div>
 </body>
